7edd013af87a462044068e73bbd8421401f6f02c
DelrosarioRovic <rovicdelrosario01@gmail.com>
2024-05-10 11:04:49 +0800

    added achievement section

---
+++ src/app/components/Achievement.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const data = [{
+    title: "30",
+    description: "Clients"
+}, {
+    title: "300+",
+    description: "Taken business legalities"
+}, {
+    title: "8",
+    description: "Years of Journey"
+}]
+
+const Achievement = () => {
+  return (
+    <div className='eventBg flex flex-col gap-10 justify-center items-center py-16'>
+        <div className='text-center text-white flex flex-col gap-5'>
+            <h1 className='text-5xl'> Some count that matters</h1>
+            <p className='text-lg'>Our achievement in the journey depicted in numbers</p>
+        </div>
+        <div className='flex gap-5 w-[500px] justify-between'>
+            {data.map((items) => (
+                <div className='flex flex-col text-center text-white gap-3'>
+                    <h2 className='text-5xl font-bold'>{items.title}</h2>
+                    <p>{items.description}</p>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Achievement
--- src/app/components/Container.jsx
+++ src/app/components/Container.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Introduction from './Introduction'
+import Introduction2 from './Introduction2'
+import Achievement from './Achievement'
 
 const Container = () => {
   return (
     <div>
         <Header />
         <Introduction />
+        <Introduction2 />
+        <Achievement />
     </div>
   )
 }
