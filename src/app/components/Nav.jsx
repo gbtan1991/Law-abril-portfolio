51b48c1b1dfaf79e786a6522abb2a215aa1104c9
DelrosarioRovic <rovicdelrosario01@gmail.com>
2024-05-15 16:20:21 +0800

    added-hover-nav

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -22,7 +22,7 @@ const Nav = () => {
     <ul className='flex gap-x-5'>
         {navItems.map((nav, index) => (
             <li key={index}>
-                <Link href={nav.url} className='uppercase text-sm'>{nav.text}</Link>
+                <Link href={nav.url} className='text-gray-500 hover:opacity-50 uppercase text-sm'>{nav.text}</Link>
             </li>
         ))}
     </ul>
