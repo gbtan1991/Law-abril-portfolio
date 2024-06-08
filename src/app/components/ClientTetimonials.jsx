64bd16f428f130889d8395b48a85e35bda37a448
Gilbert Tan <gilbert.tan1991@gmail.com>
2024-06-08 20:48:13 +0800

    First Commit

--- src/app/components/ClientTetimonials.jsx
+++ src/app/components/ClientTetimonials.jsx
@@ -12,7 +12,7 @@ const ClientTetimonials = () => {
             <div className='bg-white flex flex-col shadow-chatbox rounded-lg px-3 py-5 text-center h-[12rem] justify-center relative max-md:gap-5 
                             before:w-10 before:h-10 before:absolute before:-bottom-3 before:left-1/2 before:transform before:-translate-x-1/2 before:rotate-45 before:bg-white shadow-chatbox-before'>
               <h2 className='font-semibold text-lg text-gray-900'>Incredible Experience</h2>
-              <p className='text-sm text-gray-400'>We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly.</p>
+              <p className='text-sm text-gray-400 z-10'>We had an incredible experience working with Landify and were impressed they made such a big difference in only three weeks. Our team is so grateful for the wonderful improvements they made and their ability to get familiar with the concept so quickly.</p>
             </div>
              <div className='flex items-center gap-5'>
                 <Image alt='testimonials-image' src={testimonialsImg}/>
@@ -26,7 +26,7 @@ const ClientTetimonials = () => {
             <div className='bg-white flex flex-col shadow-chatbox rounded-lg px-3 py-5 text-center h-[12rem] justify-center relative max-md:gap-5
                             before:w-10 before:h-10 before:absolute before:-bottom-3 before:left-1/2 before:transform before:-translate-x-1/2 before:rotate-45 before:bg-white shadow-chatbox-before'>
               <h2 className='font-semibold text-lg text-gray-900'>Dependable, Responsive, Professional Partner</h2>
-              <p className='text-sm text-gray-400'>Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. </p>
+              <p className='text-sm text-gray-400 z-10'>Fermin Apps has collaborated with Landify team for several projects such as Photo Sharing Apps and Custom Social Networking Apps. The experience has been pleasant, professional and exceeding our expectations. </p>
             </div>
             <div className='flex items-center gap-5'>
               <Image alt='testimonials-image' src={testimonialsImg}/>
